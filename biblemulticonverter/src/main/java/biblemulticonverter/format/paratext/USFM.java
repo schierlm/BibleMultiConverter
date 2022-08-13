schierlm/BibleMultiConverter

commit 891a99692fcdc0ac8cc0b57c60ca07fa53c249e7

Add USX3 Import and Export, added chapter/verse end handling for USFM 2, USX 2 and USFX  #38

biblemulticonverter/src/main/java/biblemulticonverter/format/paratext/USFM.java
@@ -16,6 +16,9 @@ import biblemulticonverter.format.paratext.ParatextCharacterContent.Reference;
 import biblemulticonverter.format.paratext.ParatextCharacterContent.VerseStart;
 import biblemulticonverter.format.paratext.model.ChapterIdentifier;
 import biblemulticonverter.format.paratext.model.VerseIdentifier;
+import biblemulticonverter.format.paratext.model.Version;
+import biblemulticonverter.format.paratext.utilities.ImportUtilities;
+import biblemulticonverter.format.paratext.utilities.StandardExportLogMessages;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -52,10 +55,14 @@ public class USFM extends AbstractParatextFormat {
 
 	public static final Set<String> KNOWN_CHARACTER_TAGS = new HashSet<>(Arrays.asList("f", "fe", "x"));
 
+	private static final Set<ParagraphKind> USFM_2_PARAGRAPH_KINDS = ParagraphKind.allForVersion(Version.V2_2);
+	private static final Set<AutoClosingFormattingKind> USFM_2_AUTO_CLOSING_FORMATTING_KINDS = AutoClosingFormattingKind.allForVersion(Version.V2_2);
 
 	public static final Map<String, ParagraphKind> PARAGRAPH_TAGS = ParagraphKind.allTags();
 	public static final Map<String, FootnoteXrefKind> FOOTNOTE_XREF_TAGS = FootnoteXrefKind.allTags();
 	public static final Map<String, AutoClosingFormattingKind> AUTO_CLOSING_TAGS = AutoClosingFormattingKind.allTags();
+
+	private final StandardExportLogMessages logger = new StandardExportLogMessages("USFM 2");
 
 	@Override
 	protected ParatextBook doImportBook(File inputFile) throws Exception {
@@ -66,7 +73,7 @@ public class USFM extends AbstractParatextFormat {
 		KNOWN_CHARACTER_TAGS.addAll(AUTO_CLOSING_TAGS.keySet());
 		if (!inputFile.getName().toLowerCase().endsWith(".usfm") && !inputFile.getName().toLowerCase().endsWith(".sfm"))
 			return null;
-		String data = new String(Files.readAllBytes(inputFile.toPath()), charset).replaceAll("[\\p{Cc}\\p{Z}]+", " ").trim() + "\\$EOF$";
+		String data = new String(Files.readAllBytes(inputFile.toPath()), charset).replaceAll("[\\p{Cc}]+", " ").trim() + "\\$EOF$";
 		if (!data.startsWith("\\id ")) {
 			System.out.println("WARNING: Skipping malformed file " + inputFile);
 			return null;
@@ -82,6 +89,10 @@ public class USFM extends AbstractParatextFormat {
 		ParatextBook result = new ParatextBook(id, idParts.length == 1 ? "" : idParts[1]);
 		List<ParatextCharacterContentContainer> containerStack = new ArrayList<>();
 		boolean ignoreAutoClosingTags = Boolean.getBoolean("biblemulticonverter.usfm.ignoreautoclosingtags");
+
+		VerseStart openVerse = null;
+		ChapterStart openChapter = null;
+
 		while (startPos < finalPos) {
 			if (data.charAt(startPos) != '\\')
 				throw new IllegalStateException();
@@ -106,7 +117,13 @@ public class USFM extends AbstractParatextFormat {
 			}
 			boolean closeCharacterAttributes = false;
 			if (PARAGRAPH_TAGS.containsKey(tag)) {
-				result.getContent().add(new ParagraphStart(PARAGRAPH_TAGS.get(tag)));
+
+				ParagraphKind kind = PARAGRAPH_TAGS.get(tag);
+				//if (kind.getCategory() != ParatextBook.ParagraphKindCategory.TEXT) {
+				// Close any open verse
+				//	openVerse = closeOpenVerse(result, openVerse, false);
+				//}
+				result.getContent().add(new ParagraphStart(kind));
 				closeCharacterAttributes = true;
 			} else if (tag.endsWith("*")) {
 				String rawTag = tag.substring(0, tag.length() - 1);
@@ -173,8 +190,9 @@ public class USFM extends AbstractParatextFormat {
 					}
 				}
 			} else if (tag.equals("v")) {
-				String[] parts = textPart.split(" ", 2);
+				ImportUtilities.closeOpenVerse(result, openVerse);
 
+				String[] parts = textPart.split(" ", 2);
 				ChapterStart chapter = result.findLastBookContent(ChapterStart.class);
 				if (chapter == null) {
 					throw new IllegalStateException("Verse \\v found before chapter start milestone");
@@ -182,15 +200,29 @@ public class USFM extends AbstractParatextFormat {
 
 				// A verse number in USFM 2 may be in the format 6-7, 6a or even 6-7a.
 				// Attempt to parse these numbers by first adding the book and chapter and then parsing it as a whole.
-				VerseIdentifier location = VerseIdentifier.fromStringOrThrow(chapter.getLocation() + ":" + parts[0]);
+				VerseIdentifier location = VerseIdentifier.fromStringOrThrow(openChapter.getLocation() + ":" + parts[0]);
 
-				containerStack.get(containerStack.size() - 1).getContent().add(new VerseStart(location, parts[0]));
+				openVerse = new VerseStart(location, parts[0]);
+
+				containerStack.get(containerStack.size() - 1).getContent().add(openVerse);
 				textPart = parts.length == 1 ? "" : parts[1];
 			} else if (tag.equals("c")) {
+
+				ImportUtilities.closeOpenVerse(result, openVerse);
+				openVerse = null;
+
+				// There is not really a good way to accurately determine where the end of a chapter should be placed
+				// based on USFM 2 content. Maybe a title above this chapter marker was already intended to be part of
+				// this chapter. This is basically a best guess. This should not really matter when converting from
+				// USFM 2 to USX 2 or USFX (which is based on USFM 2), however when up-converting to USX 3 this might
+				// lead to unexpected results.
+				ImportUtilities.closeOpenChapter(result, openChapter);
+
 				String[] parts = textPart.split(" ", 2);
 				if (!parts[0].matches("[0-9]+"))
 					throw new NumberFormatException("Invalid chapter number in \\c " + textPart);
-				result.getContent().add(new ChapterStart(new ChapterIdentifier(id, Integer.parseInt(parts[0]))));
+				openChapter = new ChapterStart(new ChapterIdentifier(id, Integer.parseInt(parts[0])));
+				result.getContent().add(openChapter);
 				closeCharacterAttributes = true;
 				textPart = parts.length == 1 ? "" : parts[1];
 			} else if (tag.matches("t[hc]r?[0-9]+")) {
@@ -223,6 +255,8 @@ public class USFM extends AbstractParatextFormat {
 						throw new IOException("Two charsets specified: " + charset + " and " + correctCharset);
 					}
 					return doImportBook(inputFile, correctCharset);
+				} else {
+					result.getAttributes().put(tag, textPart);
 				}
 				textPart = "";
 			} else if (BOOK_HEADER_ATTRIBUTE_TAGS.contains(tag)) {
@@ -244,6 +278,8 @@ public class USFM extends AbstractParatextFormat {
 				containerStack.get(containerStack.size() - 1).getContent().add(new ParatextCharacterContent.Text(textPart));
 			}
 		}
+		ImportUtilities.closeOpenVerse(result, openVerse);
+		ImportUtilities.closeOpenChapter(result, openChapter);
 		return result;
 	}
 
@@ -256,7 +292,7 @@ public class USFM extends AbstractParatextFormat {
 			}
 			book.accept(new ParatextBookContentVisitor<IOException>() {
 
-				private USFMExportContext context = new USFMExportContext();
+				private USFMExportContext context = new USFMExportContext(logger);
 
 				@Override
 				public void visitChapterStart(ChapterIdentifier location) throws IOException {
@@ -271,8 +307,36 @@ public class USFM extends AbstractParatextFormat {
 
 				@Override
 				public void visitParagraphStart(ParagraphKind kind) throws IOException {
-					bw.write("\n\\" + kind.getTag());
-					context.needSpace = true;
+					if (USFM_2_PARAGRAPH_KINDS.contains(kind)) {
+						bw.write("\n\\" + kind.getTag());
+						context.needSpace = true;
+					} else {
+						visitUnsupportedParagraphStart(kind);
+					}
+				}
+
+				private void visitUnsupportedParagraphStart(ParagraphKind kind) throws IOException {
+					if (kind == ParagraphKind.HEBREW_NOTE) {
+						// According to documentation this is very similar to `d` (ParagraphKind.DESCRIPTIVE_TITLE)
+						logger.logReplaceWarning(kind, ParagraphKind.DESCRIPTIVE_TITLE);
+						visitParagraphStart(ParagraphKind.DESCRIPTIVE_TITLE);
+					} else if (kind.isSameBase(ParagraphKind.SEMANTIC_DIVISION)) {
+						// TODO maybe add more than 1 blank line?
+						logger.logReplaceWarning(kind, ParagraphKind.BLANK_LINE);
+						visitParagraphStart(ParagraphKind.BLANK_LINE);
+					} else if (kind == ParagraphKind.PARAGRAPH_PO || kind == ParagraphKind.PARAGRAPH_LH || kind == ParagraphKind.PARAGRAPH_LF) {
+						logger.logReplaceWarning(kind, ParagraphKind.PARAGRAPH_P);
+						visitParagraphStart(ParagraphKind.PARAGRAPH_P);
+					} else if (kind.getTag().startsWith(ParagraphKind.PARAGRAPH_LIM.getTag())) {
+						// Documentation is not entirely clear on what the exact difference is between `lim#` and `li#`
+						// one is "embedded" the other is not: https://ubsicap.github.io/usfm/lists/index.html#lim
+						// The assumption is made here that `lim#` is directly replaceable with `li#`
+						ParagraphKind replacement = ParagraphKind.PARAGRAPH_LI.getWithNumber(kind.getNumber());
+						logger.logReplaceWarning(kind, replacement);
+						visitParagraphStart(replacement);
+					} else {
+						throw new RuntimeException("Could not export to USFM 2 because an unhandled paragraph type `" + kind + "` from a newer USFM/USX version was found.");
+					}
 				}
 
 				@Override
@@ -299,7 +363,12 @@ public class USFM extends AbstractParatextFormat {
 	}
 
 	private static class USFMExportContext {
+		StandardExportLogMessages logger;
 		boolean needSpace = false;
+
+		public USFMExportContext(StandardExportLogMessages logger) {
+			this.logger = logger;
+		}
 	}
 
 	private static class USFMCharacterContentVisitor implements ParatextCharacterContentVisitor<IOException> {
@@ -336,24 +405,79 @@ public class USFM extends AbstractParatextFormat {
 
 		@Override
 		public ParatextCharacterContentVisitor<IOException> visitAutoClosingFormatting(AutoClosingFormattingKind kind, Map<String, String> attributes) throws IOException {
-			if (context.needSpace)
-				bw.write(" ");
-			AutoClosingFormattingKind lastTag = getLastTag();
-			String thisTag = (lastTag != null ? "+" : "") + kind.getTag();
-			bw.write("\\" + thisTag);
-			context.needSpace = true;
-			if (attributes.isEmpty()) {
-				pushSuffix(thisTag);
-			} else {
-				StringBuilder attrs = new StringBuilder("");
-				for (Map.Entry<String, String> entry : attributes.entrySet()) {
-					if (attrs.length() > 0)
-						attrs.append(" ");
-					attrs.append(entry.getKey() + "=\"" + entry.getValue() + "\"");
+			if (USFM_2_AUTO_CLOSING_FORMATTING_KINDS.contains(kind)) {
+				if (context.needSpace)
+					bw.write(" ");
+				AutoClosingFormattingKind lastTag = getLastTag();
+				String thisTag = (lastTag != null ? "+" : "") + kind.getTag();
+				bw.write("\\" + thisTag);
+				context.needSpace = true;
+				if (attributes.isEmpty()) {
+					pushSuffix(thisTag);
+				} else {
+					// TODO it can happen that newer attributes are unintentionally exported
+					StringBuilder attrs = new StringBuilder("");
+					for (Map.Entry<String, String> entry : attributes.entrySet()) {
+						if (attrs.length() > 0)
+							attrs.append(" ");
+						attrs.append(entry.getKey() + "=\"" + entry.getValue() + "\"");
+					}
+					pushSuffix(thisTag + "\t|" + attrs.toString());
 				}
-				pushSuffix(thisTag + "\t|" + attrs.toString());
+			} else {
+				return visitUnsupportedAutoClosingFormatting(kind, attributes);
 			}
 			return this;
+		}
+
+		private ParatextCharacterContentVisitor<IOException> visitUnsupportedAutoClosingFormatting(AutoClosingFormattingKind kind, Map<String, String> attributes) throws IOException {
+			if (kind == AutoClosingFormattingKind.LIST_TOTAL || kind == AutoClosingFormattingKind.LIST_KEY || kind.isSameBase(AutoClosingFormattingKind.LIST_VALUE)) {
+				// It should not be too much of an issue to just skip these list tags
+				// E.g.
+				// \li1 \lik Reuben\lik* \liv1 Eliezer son of Zichri\liv1*
+				// Wil become:
+				// \li1 Reuben Eliezer son of Zichri
+				context.logger.logSkippedWarning(kind);
+				return new USFMCharacterContentVisitor(bw, context);
+			} else if (kind == AutoClosingFormattingKind.FOOTNOTE_WITNESS_LIST) {
+				// The Footnote witness list is just extra markup found within a footnote, however according to
+				// documentation found here: https://ubsicap.github.io/usfm/v3.0.rc1/notes_basic/fnotes.html
+				// Each element within a footnote must start with it's appropriate tag. So we can't just skip this tag
+				// since it could contain text. It would be better to turn this into a text entry `ft`.
+				context.logger.logReplaceWarning(kind, AutoClosingFormattingKind.FOOTNOTE_TEXT);
+				return visitAutoClosingFormatting(AutoClosingFormattingKind.FOOTNOTE_TEXT, attributes);
+			} else if (kind == AutoClosingFormattingKind.XREF_PUBLISHED_ORIGIN) {
+				// Published cross reference origin texts do not exist in USFM 2.x
+				// There is not really a nice way to downgrade these, we cannot put the `xop` tag into `xo` because it
+				// might not follow the usual `<chapter><separator><verse>` pattern.
+				// TODO, maybe we can just write the contents to the parent target, just like FOOTNOTE_WITNESS_LIST?
+				context.logger.logRemovedWarning(kind);
+				return null;
+			} else if (kind == AutoClosingFormattingKind.XREF_TARGET_REFERENCES_TEXT) {
+				// "Target reference(s) extra / added text" does not exist in USFM 2.x
+				// We should be able to get away with just adding the raw content directly `target`.
+				context.logger.logSkippedWarning(kind);
+				return new USFMCharacterContentVisitor(bw, context);
+			} else if (kind == AutoClosingFormattingKind.SUPERSCRIPT) {
+				// There is not really a good way to represent superscript in USFM 2.x
+				// To avoid losing data, we skip the tag and just add the content directly to `target`.
+				// TODO, maybe we can use `sc` (Small caps) instead?
+				context.logger.logSkippedWarning(kind, "This might lead to text that is not separated by whitespace," +
+						"since the previous text and superscript text may not have had been separated by whitespace.");
+				return new USFMCharacterContentVisitor(bw, context);
+			} else if (kind == AutoClosingFormattingKind.ARAMAIC_WORD) {
+				// There is not really a good way to represent Aramaic words in USFM 2.x
+				// To avoid losing data, we skip the tag and just add the content directly to `target`.
+				context.logger.logSkippedWarning(kind);
+				return new USFMCharacterContentVisitor(bw, context);
+			} else if (kind == AutoClosingFormattingKind.PROPER_NAME_GEOGRAPHIC) {
+				// This marker just gives geographic names a different presentation, thus can easily be skipped without
+				// too much loss.
+				context.logger.logSkippedWarning(kind);
+				return new USFMCharacterContentVisitor(bw, context);
+			} else {
+				throw new RuntimeException("Could not export to USFM 2 because an unhandled char type `" + kind + "` from a newer USFM/USX version was found.");
+			}
 		}
 
 		@Override
@@ -365,12 +489,13 @@ public class USFM extends AbstractParatextFormat {
 		public void visitText(String text) throws IOException {
 			if (context.needSpace)
 				bw.write(" ");
-			context.needSpace = text.endsWith(" ");
-			if (context.needSpace) {
-				text = text.substring(0, text.length() - 1);
-			}
+			// context.needSpace = text.endsWith(" ");
+			// if (context.needSpace) {
+			// 	text = text.substring(0, text.length() - 1);
+			// }
 			AutoClosingFormattingKind lastTag = getLastTag();
 			bw.write(escape(text, lastTag != null && lastTag.getDefaultAttributes() != null));
+			context.needSpace = false;
 		}
 
 		@Override

biblemulticonverter/src/main/java/biblemulticonverter/format/paratext/AbstractParatextFormat.java
@@ -322,6 +322,7 @@ public abstract class AbstractParatextFormat implements RoundtripFormat {
 					VerseIdentifier location = new VerseIdentifier(pid, cnum, v.getNumber());
 					v.accept(new ParatextExportVisitor("in verse", bk.getId().isNT(), ctx, null, ParagraphKind.PARAGRAPH_P, location));
 				}
+				ctx.endChapter(cnum);
 			}
 		}
 		doExportBooks(books, exportArgs);
@@ -524,6 +525,10 @@ public abstract class AbstractParatextFormat implements RoundtripFormat {
 			book.getContent().add(new ChapterStart(new ChapterIdentifier(book.getId(), cnum)));
 			currentParagraph = null;
 			charContent = null;
+		}
+
+		public void endChapter(int cnum) {
+			book.getContent().add(new ParatextBook.ChapterEnd(new ChapterIdentifier(book.getId(), cnum)));
 		}
 
 		public void closeParagraph() {
@@ -777,6 +782,9 @@ public abstract class AbstractParatextFormat implements RoundtripFormat {
 
 		@Override
 		public boolean visitEnd() {
+			if (verseLocation != null) {
+				getCharContent().getContent().add(new ParatextCharacterContent.VerseEnd(verseLocation));
+			}
 			return false;
 		}
 	}

biblemulticonverter/src/main/java/biblemulticonverter/format/paratext/ParatextDump.java
@@ -63,6 +63,9 @@ public class ParatextDump extends AbstractParatextFormat {
 						break;
 					case "CHAPTER":
 						currentBook.getContent().add(new ChapterStart(new ChapterIdentifier(currentBook.getId(),Integer.parseInt(parts[1]))));
+					case "CHAPTER-END":
+						currentBook.getContent().add(new ParatextBook.ChapterEnd(ChapterIdentifier.fromLocationString(parts[1])));
+						break;
 					case "PARAGRAPH":
 						currentBook.getContent().add(new ParagraphStart(Objects.requireNonNull(allParagraphKinds.get(parts[1]))));
 						break;
@@ -89,6 +92,9 @@ public class ParatextDump extends AbstractParatextFormat {
 		switch (parts[0]) {
 			case "VERSE":
 				target.add(new VerseStart(VerseIdentifier.fromStringOrThrow(parts[2]), parts[3]));
+				break;
+			case "VERSE-END":
+				target.add(new ParatextCharacterContent.VerseEnd(VerseIdentifier.fromStringOrThrow(parts[2])));
 				break;
 			case "FOOTNOTE":
 				FootnoteXref fx = new FootnoteXref(Objects.requireNonNull(FootnoteXrefKind.allTags().get(parts[1])), parts[2]);
@@ -155,7 +161,7 @@ public class ParatextDump extends AbstractParatextFormat {
 
 			@Override
 			public void visitChapterEnd(ChapterIdentifier location) throws IOException {
-				
+				bw.write("CHAPTER-END\t" + location + "\n");
 			}
 
 			@Override
@@ -223,7 +229,7 @@ public class ParatextDump extends AbstractParatextFormat {
 
 		@Override
 		public void visitVerseEnd(VerseIdentifier location) throws IOException {
-			
+			bw.write("VERSE-END\t" + location + "\n");
 		}
 	}
 }

biblemulticonverter/src/main/java/biblemulticonverter/format/paratext/USFX.java
@@ -14,6 +14,7 @@ import biblemulticonverter.format.paratext.ParatextCharacterContent.Text;
 import biblemulticonverter.format.paratext.ParatextCharacterContent.VerseStart;
 import biblemulticonverter.format.paratext.model.ChapterIdentifier;
 import biblemulticonverter.format.paratext.model.VerseIdentifier;
+import biblemulticonverter.format.paratext.utilities.ImportUtilities;
 import biblemulticonverter.schema.usfx.NoteContents;
 import biblemulticonverter.schema.usfx.ObjectFactory;
 import biblemulticonverter.schema.usfx.PType;
@@ -91,7 +92,12 @@ public class USFX extends AbstractParatextFormat {
 		return result;
 	}
 
+	private static class ImportBookContext {
+		ChapterStart openChapter = null;
+	}
+	
 	private ParatextBook parseBook(Book book) {
+		
 		String bookID = book.getId();
 		if (!book.getContent().isEmpty() && book.getContent().get(0) instanceof JAXBElement<?> && ((JAXBElement<?>) book.getContent().get(0)).getName().getLocalPart().equals("id")) {
 			Usfx.Book.Id id = (Usfx.Book.Id) ((JAXBElement<?>) book.getContent().remove(0)).getValue();
@@ -105,11 +111,13 @@ public class USFX extends AbstractParatextFormat {
 		}
 		ParatextBook result = new ParatextBook(id, idParts.length == 1 ? "" : idParts[1]);
 		List<ParatextCharacterContentContainer> containerStack = new ArrayList<>();
-		parseElements(result, containerStack, book.getContent());
+		ImportBookContext context = new ImportBookContext();
+		parseElements(result, containerStack, book.getContent(), context);
+		ImportUtilities.closeOpenChapter(result, context.openChapter);
 		return result;
 	}
 
-	private void parseElements(ParatextBook result, List<ParatextCharacterContentContainer> containerStack, List<Serializable> elements) {
+	private void parseElements(ParatextBook result, List<ParatextCharacterContentContainer> containerStack, List<Serializable> elements, ImportBookContext context) {
 		for (int i = 0; i < elements.size() - 1; i++) {
 			Serializable s1 = elements.get(i);
 			Serializable s2 = elements.get(i + 1);
@@ -134,14 +142,14 @@ public class USFX extends AbstractParatextFormat {
 				}
 				containerStack.get(containerStack.size() - 1).getContent().add(new Text(normalize(s.toString())));
 			} else if (s instanceof JAXBElement<?>) {
-				parseElement(result, containerStack, (JAXBElement<?>) s);
+				parseElement(result, containerStack, (JAXBElement<?>) s, context);
 			} else {
 				System.out.println("WARNING: Skipping unsupported content inside of book " + s);
 			}
 		}
 	}
 
-	private void parseElement(ParatextBook result, List<ParatextCharacterContentContainer> containerStack, JAXBElement<?> element) {
+	private void parseElement(ParatextBook result, List<ParatextCharacterContentContainer> containerStack, JAXBElement<?> element, ImportBookContext context) {
 		String localName = element.getName().getLocalPart();
 		if (localName.equals("rem") || localName.equals("cl")) {
 			result.getAttributes().put(localName, normalize((String) element.getValue()));
@@ -158,12 +166,13 @@ public class USFX extends AbstractParatextFormat {
 			}
 			result.getContent().add(new ParagraphStart(kind));
 			containerStack.clear();
-			parseElements(result, containerStack, pt.getContent());
+			parseElements(result, containerStack, pt.getContent(), context);
 		} else if (Arrays.asList("sectionBoundary", "ca", "milestone", "va", "fm", "fig", "gw", "cs", "wr").contains(localName)) {
 			System.out.println("WARNING: Skipping unsupported tag: " + localName);
-		} else if (Arrays.asList("generated", "ve", "cp", "vp", "wtp", "da", "fs").contains(localName)) {
+		} else if (Arrays.asList("generated", "cp", "vp", "wtp", "da", "fs").contains(localName)) {
 			// to be skipped
 		} else if (localName.equals("c")) {
+			ImportUtilities.closeOpenChapter(result, context.openChapter);
 			String id;
 			if (element.getValue() instanceof Usfx.Book.C) {
 				Usfx.Book.C c = (Usfx.Book.C) element.getValue();
@@ -174,7 +183,8 @@ public class USFX extends AbstractParatextFormat {
 			} else {
 				throw new IllegalStateException(element.getValue().getClass().getName());
 			}
-			result.getContent().add(new ChapterStart(new ChapterIdentifier(result.getId(), Integer.parseInt(id))));
+			context.openChapter = new ChapterStart(new ChapterIdentifier(result.getId(), Integer.parseInt(id)));
+			result.getContent().add(context.openChapter);
 			containerStack.clear();
 		} else if (localName.equals("toc")) {
 			StyledString ss = (StyledString) element.getValue();
@@ -186,7 +196,7 @@ public class USFX extends AbstractParatextFormat {
 				for (JAXBElement<PType> cell : tr.getThOrThrOrTc()) {
 					result.getContent().add(new TableCellStart(cell.getName().getLocalPart() + cell.getValue().getLevel()));
 					containerStack.clear();
-					parseElements(result, containerStack, cell.getValue().getContent());
+					parseElements(result, containerStack, cell.getValue().getContent(), context);
 				}
 			}
 		} else if (localName.equals("table") && element.getValue() instanceof PType.Table) {
@@ -196,7 +206,7 @@ public class USFX extends AbstractParatextFormat {
 				for (JAXBElement<PType> cell : tr.getThOrThrOrTc()) {
 					result.getContent().add(new TableCellStart(cell.getName().getLocalPart() + cell.getValue().getLevel()));
 					containerStack.clear();
-					parseElements(result, containerStack, cell.getValue().getContent());
+					parseElements(result, containerStack, cell.getValue().getContent(), context);
 				}
 			}
 		} else if (localName.equals("periph")) {
@@ -228,6 +238,17 @@ public class USFX extends AbstractParatextFormat {
 			}
 			VerseIdentifier location = new VerseIdentifier(result.getId(), chapter.getChapter(), id);
 			containerStack.get(containerStack.size() - 1).getContent().add(new VerseStart(location, id));
+		} else if(localName.equals("ve")) {
+			VerseStart start = result.findLastCharacterContent(VerseStart.class);
+			if (start == null) {
+				throw new IllegalStateException("Verse end found before verse start!");
+			}
+			if (containerStack.isEmpty()) {
+				ParatextCharacterContent container = new ParatextCharacterContent();
+				containerStack.add(container);
+				result.getContent().add(container);
+			}
+			containerStack.get(containerStack.size() - 1).getContent().add(new ParatextCharacterContent.VerseEnd(start.getLocation()));
 		} else if (Arrays.asList("f", "x", "fe").contains(localName)) {
 			NoteContents nc = (NoteContents) element.getValue();
 			String sfm = nc.getSfm();
@@ -239,7 +260,7 @@ public class USFX extends AbstractParatextFormat {
 			FootnoteXref nextContainer = new FootnoteXref(USFM.FOOTNOTE_XREF_TAGS.get(sfm), caller);
 			containerStack.get(containerStack.size() - 1).getContent().add(nextContainer);
 			containerStack.add(nextContainer);
-			parseElements(result, containerStack, nc.getContent());
+			parseElements(result, containerStack, nc.getContent(), context);
 			containerStack.remove(nextContainer);
 		} else if (Arrays.asList("fp", "fr", "fk", "fq", "fqa", "fl", "fdc", "fv", "ft", "fm", "xo", "xk", "xq", "xt", "xot", "xnt", "xdc").contains(localName)
 				|| (Arrays.asList("nd", "c", "tl", "it", "qt", "sls", "dc", "bdit", "bk", "pn", "k", "ord", "add", "bd", "sc", "wh", "wg", "wr", "wj", "cs", "em").contains(localName) && element.getValue() instanceof NoteContents)) {
@@ -251,7 +272,7 @@ public class USFX extends AbstractParatextFormat {
 			AutoClosingFormatting nextContainer = new AutoClosingFormatting(USFM.AUTO_CLOSING_TAGS.get(sfm), false);
 			containerStack.get(containerStack.size() - 1).getContent().add(nextContainer);
 			containerStack.add(nextContainer);
-			parseElements(result, containerStack, nc.getContent());
+			parseElements(result, containerStack, nc.getContent(), context);
 			containerStack.remove(nextContainer);
 		} else if (localName.equals("optionalLineBreak")) {
 			System.out.println("WARNING: Skipping optional line break");
@@ -308,7 +329,7 @@ public class USFX extends AbstractParatextFormat {
 				nextContainer.getAttributes().put("x-plural", "" + w.isPlural());
 			containerStack.get(containerStack.size() - 1).getContent().add(nextContainer);
 			containerStack.add(nextContainer);
-			parseElements(result, containerStack, w.getContent());
+			parseElements(result, containerStack, w.getContent(), context);
 			containerStack.remove(nextContainer);
 		} else if (localName.equals("quoteStart")) {
 			PType.QuoteStart qs = (PType.QuoteStart) element.getValue();
@@ -333,7 +354,7 @@ public class USFX extends AbstractParatextFormat {
 			AutoClosingFormatting nextContainer = new AutoClosingFormatting(USFM.AUTO_CLOSING_TAGS.get(sfm), false);
 			containerStack.get(containerStack.size() - 1).getContent().add(nextContainer);
 			containerStack.add(nextContainer);
-			parseElements(result, containerStack, v.getContent());
+			parseElements(result, containerStack, v.getContent(), context);
 			containerStack.remove(nextContainer);
 		} else if (Arrays.asList("pn", "ord", "no", "ndx", "wh", "wg", "ior").contains(localName)) {
 			AutoClosingFormatting nextContainer = new AutoClosingFormatting(USFM.AUTO_CLOSING_TAGS.get(localName), false);

commit 12b42ea0c75d71aedc1d78ffe30bbad7f59b50b0

Do not convert `\ide` from USFM to USX
This tag (input encoding) is irrelvant for the Bible content and should
therefore not be part of the conversion.

Also update the test cases.

Regression introduced in 891a99692fcdc0ac8cc0b57c60ca07fa53c249e7.

Fixes #67.

biblemulticonverter/src/main/java/biblemulticonverter/format/paratext/USFM.java
@@ -269,8 +269,6 @@ public class USFM extends AbstractParatextFormat {
 						throw new IOException("Two charsets specified: " + charset + " and " + correctCharset);
 					}
 					return doImportBook(inputFile, correctCharset);
-				} else {
-					result.getAttributes().put(tag, textPart);
 				}
 				textPart = "";
 			} else if (BOOK_HEADER_ATTRIBUTE_TAGS.contains(tag)) {
@@ -305,10 +303,7 @@ public class USFM extends AbstractParatextFormat {
 			bw.write("\\id " + book.getId().getIdentifier() + " " + escape(book.getBibleName(), false));
 			bw.write("\n\\ide UTF-8");
 			for (Map.Entry<String, String> attr : book.getAttributes().entrySet()) {
-				// Never write a charset other than the charset we are using to write this file
-				if (!attr.getKey().equals("ide")) {
-					bw.write("\n\\" + attr.getKey() + " " + escape(attr.getValue(), false));
-				}
+				bw.write("\n\\" + attr.getKey() + " " + escape(attr.getValue(), false));
 			}
 			book.accept(new ParatextBookContentVisitor<IOException>() {
 

biblemulticonverter/src/test/resources/usx2/GEN.usx
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <usx version="2.5">
     <book code="GEN" style="id">World English Bible (WEB)</book>
-    <para style="ide">UTF-8</para>
     <para style="h">Genesis</para>
     <para style="toc1">The First Book of Moses, Commonly Called Genesis</para>
     <para style="toc2">Genesis</para>

biblemulticonverter/src/test/resources/usx3/GEN-with-only-USX3-end-milestones.usx
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <usx version="3.0">
     <book code="GEN" style="id">World English Bible (WEB)</book>
-    <para style="ide">UTF-8</para>
     <para style="h">Genesis</para>
     <para style="toc1">The First Book of Moses, Commonly Called Genesis</para>
     <para style="toc2">Genesis</para>